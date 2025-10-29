a91c8aab1ea9317cacfc74e1a3e1dd1189ca803d
Khalid Konok <khalidkonok@proton.me>
2025-10-29 17:51:40 +0600

    Add files via upload
    Added my first Solidity smart contract - HelloWorld. This marks the beginning of my Web3 and smart contract development journey.

---
+++ hello_world.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract HelloWorld{
+    string public str="Hello World, This is Khalid Konok,"
+    "Learning Solidity for a great cause.";
+}
